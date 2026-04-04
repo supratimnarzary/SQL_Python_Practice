124c7fe2adfa4d192e2bd5f26ec39cf77d42a6e0
supratimnarzary <supratimnarzary@gmail.com>
2026-04-04 10:02:05 -0500

    DataLemur SQL Q1

---
+++ problems/Histogram of Tweets.sql
@@ -0,0 +1,40 @@
+-- Assume you're given a table Twitter tweet data, write a query to obtain a histogram of tweets posted per user in 2022. Output the tweet count per user as the bucket and the number of Twitter users who fall into that bucket.
+
+-- In other words, group the users by the number of tweets they posted in 2022 and count the number of users in each group.
+
+-- tweets Table:
+-- Column Name	Type
+-- tweet_id	integer
+-- user_id	integer
+-- msg	string
+-- tweet_date	timestamp
+-- tweets Example Input:
+-- tweet_id	user_id	msg	tweet_date
+-- 214252	111	Am considering taking Tesla private at $420. Funding secured.	12/30/2021 00:00:00
+-- 739252	111	Despite the constant negative press covfefe	01/01/2022 00:00:00
+-- 846402	111	Following @NickSinghTech on Twitter changed my life!	02/14/2022 00:00:00
+-- 241425	254	If the salary is so competitive why won’t you tell me what it is?	03/01/2022 00:00:00
+-- 231574	148	I no longer have a manager. I can't be managed	03/23/2022 00:00:00
+-- Example Output:
+-- tweet_bucket	users_num
+-- 1	2
+-- 2	1
+-- Explanation:
+-- Based on the example output, there are two users who posted only one tweet in 2022, and one user who posted two tweets in 2022. The query groups the users by the number of tweets they posted and displays the number of users in each group.
+
+-- Solution1:
+with bucket as (SELECT count(tweet_id) as tweet_bucket, user_id
+FROM tweets
+where Year(tweet_date) = 2022
+Group By user_id)
+
+SELECT tweet_bucket, count(user_id) as users_num from bucket
+group by tweet_bucket;
+
+-- Solution2:
+select b.tweet_bucket, count(b.user_id) as users_num 
+  FROM(SELECT count(tweet_id) as tweet_bucket, user_id , tweet_date
+  FROM tweets
+  where Year(tweet_date) = 2022
+  Group By user_id) b
+GROUP by b.tweet_bucket;
