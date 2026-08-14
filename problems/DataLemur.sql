938a9b13b14a4d25c153cc99daa1aac7727528ae
supratimnarzary <supratimnarzary@gmail.com>
2026-08-13 21:52:26 -0500

    Q35

--- problems/DataLemur.sql
+++ problems/DataLemur.sql
@@ -1528,3 +1528,48 @@ SELECT user_id, song_id, sum(song_plays) as song_plays from total_plays
 GROUP BY user_id, song_id
 ORDER BY song_plays DESC;
 
+-- Q 35:
+
+-- A Microsoft Azure Supercloud customer is defined as a customer who has purchased at least one product from every product category listed in the products table.
+
+-- Write a query that identifies the customer IDs of these Supercloud customers.
+-- customer_contracts Table:
+-- Column Name	Type
+-- customer_id	integer
+-- product_id	integer
+-- amount	integer
+-- customer_contracts Example Input:
+-- customer_id	product_id	amount
+-- 1	1	1000
+-- 1	3	2000
+-- 1	5	1500
+-- 2	2	3000
+-- 2	6	2000
+-- products Table:
+-- Column Name	Type
+-- product_id	integer
+-- product_category	string
+-- product_name	string
+-- products Example Input:
+-- product_id	product_category	product_name
+-- 1	Analytics	Azure Databricks
+-- 2	Analytics	Azure Stream Analytics
+-- 4	Containers	Azure Kubernetes Service
+-- 5	Containers	Azure Service Fabric
+-- 6	Compute	Virtual Machines
+-- 7	Compute	Azure Functions
+-- Example Output:
+-- customer_id
+-- 1
+-- Explanation:
+
+-- Customer 1 bought from Analytics, Containers, and Compute categories of Azure, and thus is a Supercloud customer. Customer 2 isn't a Supercloud customer, since they don't buy any container services from Azure.
+
+-- Solution:
+SELECT customer_id FROM
+(SELECT c.customer_id, p.product_category FROM customer_contracts c
+  JOIN products p
+  ON c.product_id = p.product_id) cte
+GROUP BY customer_id
+HAVING count(DISTINCT product_category) =
+  (SELECT count(DISTINCT product_category) FROM products )
