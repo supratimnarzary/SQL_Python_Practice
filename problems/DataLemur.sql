c0e693d6fb942516301b8cc597e3a493777940e0
supratimnarzary <supratimnarzary@gmail.com>
2026-08-18 18:11:08 -0500

    Q37

--- problems/DataLemur.sql
+++ problems/DataLemur.sql
@@ -1632,3 +1632,65 @@ from orders
 CROSS JOIN totals
 
 ORDER BY corrected_order_id;
+
+Q37:
+
+-- The Bloomberg terminal is the go-to resource for financial professionals, offering convenient access to a wide array of financial datasets. As a Data Analyst at Bloomberg, you have access to historical data on stock performance.
+
+-- Currently, you're analyzing the highest and lowest open prices for each FAANG stock by month over the years.
+
+-- For each FAANG stock, display the ticker symbol, the month and year ('Mon-YYYY') with the corresponding highest and lowest open prices (refer to the Example Output format). Ensure that the results are sorted by ticker symbol.
+-- stock_prices Schema:
+-- Column Name	Type	Description
+-- date	datetime	The specified date (mm/dd/yyyy) of the stock data.
+-- ticker	varchar	The stock ticker symbol (e.g., AAPL) for the corresponding company.
+-- open	decimal	The opening price of the stock at the start of the trading day.
+-- high	decimal	The highest price reached by the stock during the trading day.
+-- low	decimal	The lowest price reached by the stock during the trading day.
+-- close	decimal	The closing price of the stock at the end of the trading day.
+-- stock_prices Example Input:
+
+-- Note that the table below displays randomly selected AAPL data.
+-- date	ticker	open	high	low	close
+-- 01/31/2023 00:00:00	AAPL	142.28	142.70	144.34	144.29
+-- 02/28/2023 00:00:00	AAPL	146.83	147.05	149.08	147.41
+-- 03/31/2023 00:00:00	AAPL	161.91	162.44	165.00	164.90
+-- 04/30/2023 00:00:00	AAPL	167.88	168.49	169.85	169.68
+-- 05/31/2023 00:00:00	AAPL	176.76	177.33	179.35	177.25
+-- Example Output:
+-- ticker	highest_mth	highest_open	lowest_mth	lowest_open
+-- AAPL	May-2023	176.76	Jan-2023	142.28
+
+-- Solution:
+
+WITH highest as
+  (SELECT
+    ticker,
+    MAX(open) as highest_open,
+    TO_CHAR(date, 'Mon-YYYY') as highest_mth,
+    ROW_NUMBER() OVER(PARTITION BY ticker ORDER BY open DESC) as rnk
+    FROM stock_prices
+  
+  GROUP BY ticker,TO_CHAR(date, 'Mon-YYYY'), open),
+
+lowest as
+
+ (SELECT
+    ticker,
+    MIN(open) as lowest_open,
+    TO_CHAR(date, 'Mon-YYYY') as lowest_mth,
+    ROW_NUMBER() OVER(PARTITION BY ticker ORDER BY open ASC) as rnk
+    FROM stock_prices
+  
+  GROUP BY ticker,TO_CHAR(date, 'Mon-YYYY'), open)
+
+  
+  
+  SELECT h.ticker, h.highest_mth, h.highest_open, l.lowest_mth, l.lowest_open
+  FROM highest h
+  INNER JOIN lowest l
+  ON h.ticker = l.ticker
+  AND h.rnk =1
+  AND l.rnk =1
+  ORDER by h.ticker
+  
