e0bd941f198fc862b2dabb6af3aa434e70d2a878
Supratim Narzary <supratimnarzary@gmail.com>
2026-08-23 21:24:53 -0500

    Q42

--- problems/DataLemur.sql
+++ problems/DataLemur.sql
@@ -1850,3 +1850,88 @@ SELECT card_name, issued_amount
 FROM cte
 WHERE rnk = 1
 ORDER BY issued_amount DESC;
+
+-- Q42:
+-- A phone call is considered an international call when the person calling is in a different country than the person receiving the call.
+
+-- What percentage of phone calls are international? Round the result to 1 decimal.
+
+-- Assumption:
+
+--     The caller_id in phone_info table refers to both the caller and receiver.
+
+-- phone_calls Table:
+-- Column Name	Type
+-- caller_id	integer
+-- receiver_id	integer
+-- call_time	timestamp
+-- phone_calls Example Input:
+-- caller_id	receiver_id	call_time
+-- 1	2	2022-07-04 10:13:49
+-- 1	5	2022-08-21 23:54:56
+-- 5	1	2022-05-13 17:24:06
+-- 5	6	2022-03-18 12:11:49
+-- phone_info Table:
+-- Column Name	Type
+-- caller_id	integer
+-- country_id	integer
+-- network	integer
+-- phone_number	string
+-- phone_info Example Input:
+-- caller_id	country_id	network	phone_number
+-- 1	US	Verizon	+1-212-897-1964
+-- 2	US	Verizon	+1-703-346-9529
+-- 3	US	Verizon	+1-650-828-4774
+-- 4	US	Verizon	+1-415-224-6663
+-- 5	IN	Vodafone	+91 7503-907302
+-- 6	IN	Vodafone	+91 2287-664895
+-- Example Output:
+-- international_calls_pct
+-- 50.0
+-- Explanation
+
+-- There is a total of 4 calls with 2 of them being international calls (from caller_id 1 => receiver_id 5, and caller_id 5 => receiver_id 1). Thus, 2/4 = 50.0%
+
+-- Solution:
+
+Stupid Solution:
+
+WITH caller AS
+
+(SELECT DISTINCT c1.caller_id, i1.country_id FROM phone_calls c1
+  INNER JOIN phone_info i1
+  ON c1.caller_id = i1.caller_id
+  ORDER BY c1.caller_id),
+
+receiver AS
+(SELECT DISTINCT c2.receiver_id, i2.country_id FROM phone_calls c2
+  INNER JOIN phone_info i2
+  ON c2.receiver_id = i2.caller_id
+  ORDER BY c2.receiver_id),
+
+  combined AS
+  (SELECT pc.caller_id, pc.receiver_id,
+  ca.country_id AS caller_country, re.country_id AS receiver_country 
+  FROM phone_calls pc
+
+  INNER JOIN caller ca
+  ON pc.caller_id = ca.caller_id
+  INNER JOIN receiver re
+  ON pc.receiver_id = re.receiver_id)
+  
+  SELECT
+  
+  ROUND(SUM(CASE WHEN caller_country != receiver_country THEN 1 ELSE 0 END)*100.0/
+  (SELECT COUNT(caller_id) FROM combined),1) AS international_calls_pct
+  
+  FROM combined;
+
+  Good Solution:
+
+SELECT ROUND(
+         100.0 * SUM(CASE WHEN ci.country_id <> ri.country_id THEN 1 ELSE 0 END)
+         / COUNT(*),
+       1) AS international_calls_pct
+FROM phone_calls pc
+JOIN phone_info ci ON pc.caller_id   = ci.caller_id
+JOIN phone_info ri ON pc.receiver_id = ri.caller_id;
